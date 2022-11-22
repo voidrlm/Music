2a0c3c36fe5febf1b34044272fec7b4fd9287ff1
voidrlm <varjun609@gmail.com>
2022-11-23 03:47:02 +0530

    Added initial bottom navigation component

--- src/views/discover.vue
+++ src/views/discover.vue
@@ -36,13 +36,53 @@
         </v-btn>
       </v-card-actions>
     </v-card>
+    <v-bottom-navigation v-model="value" :background-color="color" dark shift>
+      <v-btn>
+        <span>Video</span>
+
+        <v-icon>mdi-television-play</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Music</span>
+
+        <v-icon>mdi-music-note</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Book</span>
+
+        <v-icon>mdi-book</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Image</span>
+
+        <v-icon>mdi-image</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-container>
 </template>
 <script>
 export default {
   name: "discover-component",
   data: () => ({ length: 3, onboarding: 0 }),
-  computed: {},
+  computed: {
+    color() {
+      switch (this.value) {
+        case 0:
+          return "blue-grey";
+        case 1:
+          return "teal";
+        case 2:
+          return "brown";
+        case 3:
+          return "indigo";
+        default:
+          return "blue-grey";
+      }
+    },
+  },
   methods: {
     next() {
       this.onboarding =
