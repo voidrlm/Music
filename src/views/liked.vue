34c70c625cdefcde9f7a2734f9a7e2eefe5e0cc2
voidrlm <varjun609@gmail.com>
2022-11-23 03:33:06 +0530

    Defined initial liked songs component

---
+++ src/views/liked.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container fluid>
+    <v-card-title
+      :class="
+        $vuetify.breakpoint.mdAndUp ? 'text-h4 mt-n7' : 'text-h5 justify-center'
+      "
+      >Liked</v-card-title
+    >
+  </v-container>
+</template>
+<script>
+export default {
+  name: "liked-component",
+  data: () => ({}),
+  computed: {},
+};
+</script>
