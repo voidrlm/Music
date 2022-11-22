b7a1dc72ada768bd8864966e9b2c88fd1240b51d
voidrlm <varjun609@gmail.com>
2022-11-23 03:44:00 +0530

    Added initial UI elements for discover page

--- src/views/discover.vue
+++ src/views/discover.vue
@@ -5,13 +5,53 @@
         $vuetify.breakpoint.mdAndUp ? 'text-h4 mt-n7' : 'text-h5 justify-center'
       "
       >Discover</v-card-title
-    >
+    ><v-card flat tile>
+      <v-window v-model="onboarding">
+        <v-window-item v-for="n in length" :key="`card-${n}`">
+          <v-card color="grey" height="200">
+            <v-row class="fill-height" align="center" justify="center">
+              <h1 style="font-size: 5rem" class="white--text">Slide {{ n }}</h1>
+            </v-row>
+          </v-card>
+        </v-window-item>
+      </v-window>
+
+      <v-card-actions class="justify-space-between">
+        <v-btn text @click="prev">
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-item-group v-model="onboarding" class="text-center" mandatory>
+          <v-item
+            v-for="n in length"
+            :key="`btn-${n}`"
+            v-slot="{ active, toggle }"
+          >
+            <v-btn :input-value="active" icon @click="toggle">
+              <v-icon>mdi-record</v-icon>
+            </v-btn>
+          </v-item>
+        </v-item-group>
+        <v-btn text @click="next">
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
 <script>
 export default {
   name: "discover-component",
-  data: () => ({}),
+  data: () => ({ length: 3, onboarding: 0 }),
   computed: {},
+  methods: {
+    next() {
+      this.onboarding =
+        this.onboarding + 1 === this.length ? 0 : this.onboarding + 1;
+    },
+    prev() {
+      this.onboarding =
+        this.onboarding - 1 < 0 ? this.length - 1 : this.onboarding - 1;
+    },
+  },
 };
 </script>
