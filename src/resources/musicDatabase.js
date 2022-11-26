1bdf539a43ca486158feed51b135a8262c621068
voidrlm <varjun609@gmail.com>
2022-11-27 02:39:29 +0530

    Added music database

---
+++ src/resources/musicDatabase.js
@@ -0,0 +1,33 @@
+export const musicDatabase = [
+  {
+    id: 1,
+    name: "Dior (Bonus)",
+    artist: "Pop Smoke",
+    album: "Meet The Woo 2",
+    albumArt:
+      "https://upload.wikimedia.org/wikipedia/en/1/13/Pop_Smoke_-_Meet_the_Woo_2.png",
+    url: "https://luvmp.com/wp-content/uploads/2021/09/Pop_Smoke_-_Dior_www.luvmp_.com_.mp3",
+    isExplict: true,
+    isLiked: true,
+  },
+  {
+    id: 2,
+    name: "For The Night ft Lil Baby & DaBaby",
+    artist: "Pop Smoke",
+    album: "Shoot for the Stars Aim for the Moon",
+    albumArt: "https://i1.sndcdn.com/artworks-qdmzq848isf6-0-t500x500.jpg",
+    url: "https://luvmp.com/wp-content/uploads/2021/12/Pop-Smoke-For-The-Night-Audio-ft.-Lil-Baby-DaBaby_Luvmp.com_.mp3",
+    isExplict: true,
+    isLiked: true,
+  },
+  {
+    id: 3,
+    name: "GATTI ft. Pop Smoke",
+    artist: "JACKBOYS & Travis Scott",
+    album: "JACKBOYS",
+    albumArt: "https://i1.sndcdn.com/artworks-qbGQn563MrGu-0-t500x500.jpg",
+    url: "http://www.luvmp.com/wp-content/uploads/2019/12/JACKBOYS_Travis_Scott_ft_Pop_Smoke_-_GATTI_Luvmp.com_.mp3",
+    isExplict: true,
+    isLiked: true,
+  },
+];
